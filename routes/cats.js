e6630038190daa9150ed3174e503a5fb22e3167a
Samuel Chacon <sechafer@hotmail.com>
2024-07-02 07:36:07 -0300

    fix routes 2

--- routes/cats.js
+++ routes/cats.js
@@ -3,8 +3,8 @@ const catsController = require('../controllers/cats.js');
 const validation = require('../middleware/validate.js');
 const { isAuthenticated } = require('../middleware/authenticate.js');
 
-router.get('/:id', validation.checkMongoId, catsController.getSingle);
-router.get('/', catsController.getAll);
+router.get('/:id',isAuthenticated, validation.checkMongoId, catsController.getSingle);
+router.get('/',isAuthenticated, catsController.getAll);
 router.post('/', isAuthenticated, validation.saveCat, catsController.createCat);
 router.put('/:id', isAuthenticated, validation.checkMongoId, validation.saveCat, catsController.updateCat);
 router.delete('/:id', isAuthenticated, validation.checkMongoId, catsController.deleteCat);
--- routes/motos.js
+++ routes/motos.js
@@ -3,8 +3,8 @@ const motosController = require('../controllers/motos.js');
 const validation = require('../middleware/validate.js');
 const { isAuthenticated } = require('../middleware/authenticate.js');
 
-router.get('/:id', validation.checkMongoId, motosController.getSingle);
-router.get('/', motosController.getAll);
+router.get('/:id',isAuthenticated, validation.checkMongoId, motosController.getSingle);
+router.get('/',isAuthenticated, motosController.getAll);
 router.post('/', isAuthenticated, validation.saveMoto, motosController.createMoto);
 router.put('/:id', isAuthenticated, validation.checkMongoId, validation.saveMoto, motosController.updateMoto);
 router.delete('/:id', isAuthenticated, validation.checkMongoId, motosController.deleteMoto);
